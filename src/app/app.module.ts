2f73731577733ad063e87f0d5cc116f497335207
Alejandro Lora <alejandrofpo@gmail.com>
2021-06-06 03:00:06 +0200

    high order observables with map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeModule } from './home/home.module';
 import { NotificationsModule } from './notifications/notifications.module';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
+    HomeModule,
     NotificationsModule
   ],
   declarations: [AppComponent],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import { fromEvent, Observable } from 'rxjs';
+import { tap, map, concatMap, mergeMap, switchMap, exhaustMap } from 'rxjs/operators';
+import { PokemonService } from '../_shared/services/pokemon.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  pokemon: any;
+  search$!: Observable<any>;
+
+  @ViewChild('input', { static: true }) input!: ElementRef<HTMLInputElement>;
+
+  constructor(private readonly pokemonService: PokemonService) { }
 
   ngOnInit(): void {
+    this.search$ = fromEvent(this.input.nativeElement, 'keyup').pipe(
+      tap(() => this.pokemon = null), // Remove latest pokemon
+      map(e => (e.target as HTMLInputElement).value), // Map to get input value from event
+      // concatMap, mergeMap, switchMap, exhaustMap => high order observables
+      // concatMap(pokemonId => this.pokemonService.getPokemon(pokemonId)) // wait for prev stream to complete before emit new one
+      // mergeMap(pokemonId => this.pokemonService.getPokemon(pokemonId)) // emit new value as soon as it comes (warning: race conditions)
+      // switchMap(pokemonId => this.pokemonService.getPokemon(pokemonId)) // cancel previous stream if not completed
+      exhaustMap(pokemonId => this.pokemonService.getPokemon(pokemonId)) // not emit inner obs value until prev stream finish
+    );
+    this.search$.subscribe(pokemon => this.pokemon = pokemon);
   }
 
 }
