04a57fa3852f8d89a1f98cb781408ce283534f90
Alejandro Lora <alejandrofpo@gmail.com>
2021-06-07 18:35:07 +0200

    async await / try catch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
-import { EMPTY, fromEvent, Observable } from 'rxjs';
-import { tap, map, switchMap, catchError, concatMap } from 'rxjs/operators';
 import { PokemonService } from '../_shared/services/pokemon.service';
 
 @Component({
@@ -12,29 +10,23 @@ import { PokemonService } from '../_shared/services/pokemon.service';
 export class HomeComponent implements OnInit {
 
   pokemon: any;
-  search$!: Observable<any>;
 
   @ViewChild('input', { static: true }) input!: ElementRef<HTMLInputElement>;
 
   constructor(private readonly pokemonService: PokemonService) { }
 
-  ngOnInit(): void {
-    this.search$ = fromEvent(this.input.nativeElement, 'keyup').pipe(
-      tap(() => this.pokemon = null),
-      map(e => (e.target as HTMLInputElement).value),
-      switchMap(pokemonId => this.pokemonService.getPokemon(pokemonId)
-        .pipe(
-          catchError(error => { // If use it on the inner observable, keep emitting...
-            console.log('ERROR', error);
-            return EMPTY;
-          }))
-      ),
-      // catchError(error => { // If use it on the high order observable, stop emitting...
-      //   console.log('ERROR', error);
-      //   return EMPTY;
-      // })
-    );
-    this.search$.subscribe(pokemon => this.pokemon = pokemon);
-  }
+  async ngOnInit(): Promise<void> {
 
+    const pokemon1 = await this.pokemonService.getPokemon('1').toPromise();
+
+    try {
+      const pokemon2 = await this.pokemonService.getPokemon('yo').toPromise();
+      console.log(pokemon2);
+    } catch (error) {
+      console.log('ERROR', error);
+    }
+
+    this.pokemon = pokemon1;
+    console.log(pokemon1);
+  }
 }
