d4670572e254dab84ef5f1e047624ea20874b8ce
Alejandro Lora <alejandrofpo@gmail.com>
2021-06-07 21:14:26 +0200

    Solution

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
+import { fromEvent, EMPTY, Observable, } from 'rxjs';
+import { tap, map, switchMap, catchError, filter, distinctUntilChanged, debounceTime, finalize } from 'rxjs/operators';
+
 import { PokemonService } from '../_shared/services/pokemon.service';
 
 @Component({
@@ -9,24 +12,38 @@ import { PokemonService } from '../_shared/services/pokemon.service';
 })
 export class HomeComponent implements OnInit {
 
-  pokemon: any;
+  loading = false;
+  search$!: Observable<any>;
 
   @ViewChild('input', { static: true }) input!: ElementRef<HTMLInputElement>;
 
   constructor(private readonly pokemonService: PokemonService) { }
 
-  async ngOnInit(): Promise<void> {
-
-    const pokemon1 = await this.pokemonService.getPokemon('1').toPromise();
-
-    try {
-      const pokemon2 = await this.pokemonService.getPokemon('yo').toPromise();
-      console.log(pokemon2);
-    } catch (error) {
-      console.log('ERROR', error);
-    }
-
-    this.pokemon = pokemon1;
-    console.log(pokemon1);
+  ngOnInit(): void {
+    this.search$ = fromEvent(this.input.nativeElement, 'keyup').pipe(
+      // Mapping to extract the input value
+      map(e => (e.target as HTMLInputElement).value),
+      // Stop stream if input is empty
+      filter(value => !!value),
+      // Debounce of 3Sec
+      debounceTime(3000),
+      // If prev value was the same than current, stop the stream
+      distinctUntilChanged(),
+      // Loading become true after passing the conditions to fire the request
+      tap(() => this.loading = true),
+      // Cancel any prev request if before it finished, user triggers new one
+      switchMap(pokemonId => this.pokemonService.getPokemon(pokemonId)
+        .pipe(
+          // Catch possible errors due wrong input value sent to API
+          catchError(error => {
+            console.log('ERROR', error);
+            return EMPTY;
+          }),
+          // Either way, turn loading to false (switch map API request finishes, higher order obs doesn't...)
+          finalize(() => this.loading = false)
+        )
+      )
+    );
   }
+
 }
