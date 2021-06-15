605f748675f1ea3a8c28170d22220018b2e48ac6
Alejandro Lora <alejandrofpo@gmail.com>
2021-06-15 20:20:14 +0200

    router logs (events)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+
+  constructor(private readonly router: Router) {
+    this.router.events.subscribe(evt => {
+      if (evt instanceof NavigationStart) {
+        console.log('START', evt);
+      }
+      else if (evt instanceof NavigationEnd) {
+        console.log('END', evt);
+      } else {
+        console.log(evt);
+      }
+    });
+    // this.router.events.pipe(filter(evt => evt instanceof NavigationStart)).subscribe(evt => console.log(evt));
+  }
+}
